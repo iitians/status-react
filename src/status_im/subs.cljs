bf4ef8d67e9e9b1b9470c235c5ff45907c9b8531
Gheorghe Pinzaru <feross95@gmail.com>
2020-05-15 10:24:08 +0300

    f pulling

--- src/status_im/subs.cljs
+++ src/status_im/subs.cljs
@@ -2203,10 +2203,6 @@
    (not-any? :error (vals manage))))
 
 ;; Acquisition Referral reward
-(re-frame/reg-sub
- :invite/default-reward
- (fn [db]
-   (get-in db [:acquisition :referral :amount])))
 
 (re-frame/reg-sub
  :invite/accounts-reward
